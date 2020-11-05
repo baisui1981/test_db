139fcff545c1aa19be59e574dc0fca25bea07bb4
百岁 <mozhenghua19811109@126.com>
2020-11-05 16:59:39 +0800

    Update employees.sql
    去掉dept_emp 的主外键约束

--- employees.sql
+++ employees.sql
@@ -70,8 +70,8 @@ CREATE TABLE dept_emp (
     dept_no     CHAR(4)         NOT NULL,
     from_date   DATE            NOT NULL,
     to_date     DATE            NOT NULL,
-    FOREIGN KEY (emp_no)  REFERENCES employees   (emp_no)  ON DELETE CASCADE,
-    FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
+  --  FOREIGN KEY (emp_no)  REFERENCES employees   (emp_no)  ON DELETE CASCADE,
+  --  FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
     PRIMARY KEY (emp_no,dept_no)
 );
 
